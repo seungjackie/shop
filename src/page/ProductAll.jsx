0fb4f240ffc64fff2ba62f716df1d62be7124cae
seungJackie <qkrtmdwo28@gmail.com>
2023-05-02 14:53:14 +0900

    배포

--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -1,42 +1,44 @@
-import React , {useEffect, useState} from 'react'
-import ProductCard from '../components/ProductCard'
-import {Container , Row, Col}from 'react-bootstrap';
-import { useSearchParams } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+import { Container, Row, Col } from "react-bootstrap";
+import { useSearchParams } from "react-router-dom";
 
 const ProductAll = () => {
-  
-  const [productList, setProductList] = useState([])
+  const [productList, setProductList] = useState([]);
 
-  const [qeury,setQeury] = useSearchParams();
+  const [qeury, setQeury] = useSearchParams();
 
   const getProducts = async () => {
     let searchQuery = qeury.get("q") || " ";
-    console.log("쿼리값은?",searchQuery)
+    console.log("쿼리값은?", searchQuery);
     // todo
-    let url = `http://localhost:3004/products?q=${searchQuery}`;
-    let response =  await fetch(url);
+    let url = `https://my-json-server.typicode.com/seungjackie/shop/products?q=${searchQuery}`;
+    let response = await fetch(url);
     let data = await response.json();
-    console.log(data)
-    setProductList(data)
-  }
+    console.log(data);
+    setProductList(data);
+  };
 
   // 데이터 가져오기
-  useEffect(() =>{
-    getProducts()
-  } ,[qeury])
+  useEffect(() => {
+    getProducts();
+  }, [qeury]);
 
   return (
     <div>
       <Container>
-          <Row>
-            {productList.map((menu, index) => {
-                return <Col lg={3} key={index}><ProductCard item={menu}/></Col>
-            })}
-          </Row>
+        <Row>
+          {productList.map((menu, index) => {
+            return (
+              <Col lg={3} key={index}>
+                <ProductCard item={menu} />
+              </Col>
+            );
+          })}
+        </Row>
       </Container>
-    </div>  
-    )
-}
+    </div>
+  );
+};
 
-export default ProductAll
+export default ProductAll;
